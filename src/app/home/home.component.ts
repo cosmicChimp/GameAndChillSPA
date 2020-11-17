e45f555bee82e1abb3914cacf9b76f86389c4de3
cosmicChimp <ian.t.marrs@gmail.com>
2020-11-17 15:12:27 -0700

    added user service and updatd baseUrl added loadUsers() to memblist component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,22 +4,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  // tslint:disable-next-line: typedef
   registerToggle() {
-    this.registerMode = true
+    this.registerMode = true;
   }
 
+  // tslint:disable-next-line: typedef
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
   }
-
 }
