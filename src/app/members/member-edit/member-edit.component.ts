4c70fdef3e595a9ba1655b23ccb51719bfc5d7cb
cosmicChimp <ian.t.marrs@gmail.com>
2020-11-18 17:46:31 -0700

    finished edit form template and used angular forms

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,14 +10,25 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', { static: true }) editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) {}
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
     this.route.data.subscribe((data) => {
       this.user = data['user'];
     });
+  }
+
+  // tslint:disable-next-line: typedef
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successful');
+    this.editForm.reset(this.user);
   }
 }
