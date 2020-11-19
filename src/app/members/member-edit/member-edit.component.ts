059e92484780f8c2cf9ddc914f376d70932e67df
cosmicChimp <ian.t.marrs@gmail.com>
2020-11-18 17:25:45 -0700

    stubbed out edit form for member edit component

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -5,7 +5,7 @@ import { User } from 'src/app/_models/user';
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
-  styleUrls: ['./member-edit.component.scss'],
+  styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
   user: User;
